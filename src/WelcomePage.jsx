4365c83ea1934523d21612c5dbf404528f0c932b
masechkacat <masechkacat@gmail.com>
2024-02-07 13:38:56 +0100

    fix img

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -1,12 +1,13 @@
 import { Box } from '@mui/material';
 import Card from '@mui/material/Card';
+import FilePresentIcon from '@mui/icons-material/FilePresent';
 
 const WelcomePage = () => {
   return (
     <Card>
       <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent:'center', alignContent: 'center', fontFamily: 'Monospace' }}>
         <h1 style={{ display: 'block', alignSelf: 'center' }}>Welcome</h1>
-        <img src="/img/welcome.webp" alt="welcome" width={'20%'} style={{ alignSelf: 'center' }} />
+        <FilePresentIcon style={{ fontSize: '5rem', alignSelf: 'center' }} />
         <p style={{ padding: '0 21%' }}>I&apos;m glad you&apos;re here. This is a simple note-taking app. Here you can create, edit, and delete notes with markdown. To get started, click the &quot;Add New Note&quot; button on the left.</p>
       </Box>
     </Card>
